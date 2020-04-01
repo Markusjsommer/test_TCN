78153aa4cfac6a4da962b402bc9e53af05c7437c
Markus Sommer <markusjsommer@gmail.com>
2020-04-01 19:24:38 -0400

    model push

---
+++ hubconf.py
@@ -0,0 +1,11 @@
+dependencies = ['torch']
+
+def geneTCN():
+    """ Pretrained Temporal Convolutional Network for bacterial gene identification."""
+    model = 
+    dirname = os.path.dirname(__file__)
+    checkpoint = os.path.join(dirname, "weights/geneTCN.pt")
+    state_dict = torch.load(checkpoint)
+    model.load_state_dict(state_dict)
+    
+    return model
