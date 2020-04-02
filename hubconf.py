05516245f13c2442c425bfa22b907c8fd7b21493
Markus Sommer <markusjsommer@gmail.com>
2020-04-01 20:39:48 -0400

    model push

--- hubconf.py
+++ hubconf.py
@@ -37,7 +37,8 @@ class TCN(nn.Module):
     def forward(self, inputs):
         """Inputs have to have dimension (N, C_in, L_in)"""
         y1 = self.tcn(inputs)  # input should have dimension (N, C, L)
-        o = self.linear(y1[:, :, -1])
+        # o = self.linear(y1[:, :, -1])
+        o = self.linear(y1[:, :, :]) # return all outputs so we can select the correct index for the actual length of the non-padded sequence
         return o
 
 class Chomp1d(nn.Module):
